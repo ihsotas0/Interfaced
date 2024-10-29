2c498bf3e935af62ce1a53eea47383a150b1696f
Jonah Spector <jonah.m.spector@proton.me>
2024-10-29 09:39:39 -0600

    Placeholder build script
    I need to finish it for EPUB and HTML with added styling and stuff. I also need to think about how I will display it on my website.

--- build.sh
+++ build.sh
@@ -5,11 +5,28 @@ DATE=$(date '+%Y-%m-%d')
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DRAFT_TITLE="${GIT_BRANCH}_${DATE}"
 
-mkdir -p drafts/${DRAFT_TITLE}
-echo "Made directory: drafts/${DRAFT_TITLE}"
+for book in $(ls -d content/*/ | xargs -n 1 basename)
+do
+    echo "Found book: content/${book%%/}"
+    DRAFT_PATH="drafts/${DRAFT_TITLE}/${book%%/}"
+    mkdir -p ${DRAFT_PATH}
+    > ${DRAFT_PATH}/raw_content.md
+    echo "Created/cleared ${DRAFT_PATH}/raw_content.md"
+    echo "Adding content to ${DRAFT_PATH}/raw_content.md..."
+    for file in $(ls content/${book})
+    do
+        cat content/${book}/${file} >> ${DRAFT_PATH}/raw_content.md
+        echo "\n\n\n" >> ${DRAFT_PATH}/raw_content.md
+    done
+done
 
-touch drafts/${DRAFT_TITLE}/raw_content.md
-echo "Made raw content file: drafts/${DRAFT_TITLE}/raw_content.md"
+for book in $(ls -d drafts/${DRAFT_TITLE}/*/ | xargs -n 1 basename)
+do
+    echo "Compiling drafts/${DRAFT_TITLE}/${book}/JonahSpector_${book}.pdf..."
+    pandoc drafts/${DRAFT_TITLE}/${book}/raw_content.md \
+    --metadata-file content/${book}/metadata.yml \
+    -o drafts/${DRAFT_TITLE}/${book}/JonahSpector_${book}.pdf
+done
 
-
+echo "...Done!"
 
